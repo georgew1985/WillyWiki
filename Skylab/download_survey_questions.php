a40a7dd1fbf79090bb46acae3e3d7db25ab2cad5
mvwm <31545645+mvwm@users.noreply.github.com>
2018-12-02 20:47:55 +0100

    Create download_survey_questions.php

---
+++ Skylab/download_survey_questions.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<?php
+// make SSH connection
+print("Making SSH connection <br>");
+// use correct host when in production
+$connection = ssh2_connect('10.10.1.34',22);
+
+include("secret_info.php");
+$user = SSH_USERNAME;
+$pass = SSH_PASSWORD;
+
+print("Logging in <br>");
+
+// login to remote server
+if (ssh2_auth_password($connection, $user, $pass)) {
+    print("Authentication Successful!<br>");
+} else {
+    print("Authentication Failed...<br>");
+}
+
+// make SFTP connection
+print("Making SFTP connection <br>");
+$sftp = ssh2_sftp($connection);
+print("SFTP connection successful! <br>");
+
+// copy file
+// real locations when connection is made, ** un-remark next line for production **
+// $remote_answers_file = "/opt/willy/components/social_interaction/src/interactions/assets/survey_answers.csv";
+$local_answers_file  = "/home/willy/survey/survey_answers.csv";
+// $local_answers_file  = "/tmp/survey_answers.csv";
+
+// test locations, ** remove next line for production **
+$remote_answers_file = "/home/willy/survey/survey_answers.csv";
+
+print("Copying file " . $remote_answers_file . " to " . $local_answers_file . "<br>");
+
+if (ssh2_scp_recv($connection, $remote_answers_file, $local_answers_file)) {
+    print("File copy successful!<br><br>");
+} else {
+    print("<br>File copy failed...<br><br>");
+}
+
+ssh2_exec($connection,'exit');
+?>
+
+<a href="survey.html">Terug naar enquete pagina</a>
